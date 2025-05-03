ffc70e84af34a30835bea0754a445fd7a4e6eac9
Zachery Chin <zacherychin@Zacherys-MacBook-Air.local>
2025-05-03 15:37:13 -0700

    Add seed for basic setup
    as per the take home doc

--- mitti/spec/factories/properties.rb
+++ mitti/spec/factories/properties.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :property do
-    address { "MyString" }
+    address { "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.state_abbr}"}
   end
 end
