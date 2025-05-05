4dbeed7c3e487466588aad63b6c43b8c15da0d57
Zachery Chin <zacherychin@Zacherys-MacBook-Air.local>
2025-05-04 22:36:21 -0700

    remove tests. were using rspec instead

--- mitti/spec/rails_helper.rb
+++ mitti/spec/rails_helper.rb
@@ -72,6 +72,9 @@ RSpec.configure do |config|
   #
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  #config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
   config.before(:suite) do
     Devise.configure_warden!
   end
